9e872c52e68c184b81391084e7c8aa733532226c
Daren Schaad and Tyler Suderman <darenschaad@gmail.com>
2016-01-21 15:32:54 -0800

    add second spec and functionality for vowel - switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 var removeVowel = function(string){
+
   var vowels = ["a","e","i","o","u","A","E","I","O","U"]
-  for (var i =0; i < vowels.length; i ++){
-    for (var j=0; j< string.length; j ++){
-      if (string.charAt([i]) === vowels[j]){
-        return true
+  var chars = string.split("")
+  for (var i=0; i < chars.length; i ++){
+    for (var j=0; j < vowels.length; j ++){
+      if (chars[j] === vowels[i]){
+        debugger;
+        chars.splice(j, 1, "-");
       }
     }
-  }
-}
+  }return chars.join("");
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("removeVowel", function(){
   it("will detect if there is a vowel in the argument entered", function(){
     expect(removeVowel("cat")).to.equal(true);
   });
+  it("will remove a vowel from the string and replace it with '-'", function(){
+    expect(removeVowel("cat")).to.equal("c-t");
+  });
 });
